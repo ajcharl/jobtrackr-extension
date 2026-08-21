bcd96049ab818a90c484a68395feab20f1c2e3cf
charles <charles_ajie@yahoo.com>
2026-08-21 17:43:35 -0400

    updates

--- extension/config.js
+++ extension/config.js
@@ -1,3 +1,3 @@
 // Edit this file to point the extension at your deployed backend.
 // After changing, reload the extension in chrome://extensions.
-self.API_BASE = "https://jobtrackr-api-ilgr.onrender.com";
+self.API_BASE = "https://jobtrackr-extension.onrender.com";
